cbe952383c4c3bbf880d5751c6aefa9ceeebcc4c
kunaldhongade <kunaldhongade1@gmail.com>
2023-12-26 14:23:37 +0530

    chore: clean up

--- node/database/db.js
+++ node/database/db.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
@@ -8,8 +9,8 @@ const dbName = 'nodejs_course';
 
 async function connect() {
     await client.connect();
-    console.log("Connected successfully to server");
-    const db = client.db(dbName);
+    console.log("Connected successfully to database server");
+    const db = await client.db(dbName);
     return db;
 }
 
