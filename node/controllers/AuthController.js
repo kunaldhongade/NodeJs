a8c8bc778ca4b8496f195ca5d95ea0ad6a4ccab2
kunaldhongade <kunaldhongade1@gmail.com>
2023-12-27 23:07:01 +0530

    add: auth controllers

---
+++ node/controllers/AuthController.js
@@ -0,0 +1,26 @@
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+let jwt = require("jsonwebtoken")
+const saltRounds = 10;
+
+exports.signup = async (req, res) => {
+    const password = await bcrypt.hash(req.body.password, saltRounds)
+    const data = { ...req.body, password }
+    const user = await User.create(data)
+    res.json({ user })
+}
+
+exports.login = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        res.status(404).json({ error: "user not found" })
+        return;
+    }
+    if (!await bcrypt.compare(req.body.password, user.password)) {
+        res.status(401).json({ error: "password not matched" })
+        return;
+    }
+
+    const token = await jwt.sign({ user }, "fake-jwt-secret")
+    res.json({ user, access_token: token })
+}
--- node/controllers/BookController.js
+++ node/controllers/BookController.js
@@ -1,19 +1,28 @@
 const connect = require("../database/db")
 const { ObjectId } = require("mongodb")
+const Book = require("../models/Book")
 
 exports.index = async (req, res) => {
-    const db = await connect();
-    const books = await db.collection('book').find().toArray()
+    // const db = await connect();
+    // const books = await db.collection('book').find().toArray()
+    const books = await Book.find();
     res.json(books)
 }
 
 exports.create = async (req, res) => {
     // console.log(req.body)
     // res.json(req.body)
-    const db = await connect();
+    // const db = await connect();
     // await db.collection('books').insertOne(data)
-    await db.collection('book').insertOne(req.body)
-    res.status(201).json({ data: "book is stored" })
+    // await db.collection('book').insertOne(req.body)
+
+    try {
+        await Book.create(req.body)
+        res.status(201).json({ data: "book is stored" })
+    } catch (error) {
+        res.json(error.errors)
+    }
+
 }
 
 exports.put = (req, res) => {
@@ -21,24 +30,31 @@ exports.put = (req, res) => {
 }
 
 exports.show = async (req, res) => {
-    const db = await connect();
+    // const db = await connect();
     const _id = new ObjectId(req.params.id)
-    const book = await db.collection('book').find({ _id }).toArray()
+    // const book = await db.collection('book').find({ _id }).toArray()
+
+    const book = await Book.find({ _id })
     res.json(book)
     // console.log(req.params)
     // res.send(`book id is ${req.params.id}`)
 }
 
 exports.update = async (req, res) => {
-    const db = await connect();
+    // const db = await connect();
     const _id = new ObjectId(req.params.id)
-    await db.collection('book').updateOne({ _id }, { $set: req.body })
+    // await db.collection('book').updateOne({ _id }, { $set: req.body })
+
+    await Book.updateOne({ _id }, { $set: req.body })
+
     res.json({ 'data': `${_id} book is updated` })
 }
 
 exports.delete = async (req, res) => {
-    const db = await connect();
+    // const db = await connect();
     const _id = new ObjectId(req.params.id)
-    await db.collection('book').deleteOne({ _id })
+    // await db.collection('book').deleteOne({ _id })
+
+    await Book.deleteOne({ _id })
     res.status(204).json()
 }
