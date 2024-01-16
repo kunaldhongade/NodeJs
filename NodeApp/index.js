9dc406cf0dad15be79e36edba738cf8f17e98204
kunaldhongade <kunaldhongade1@gmail.com>
2024-01-16 18:15:09 +0530

    update: index.js

--- NodeApp/index.js
+++ NodeApp/index.js
@@ -1,2 +1,10 @@
+// const lib = require('./lib.js')
+import { add, diff, div, mul } from './lib2.js';
+
+console.log(add(5, 6), mul(2, 3), diff(5, 6), div(10, 2))
+
 const a = 5;
+
+// every file in node is module
+// all property is protected my module
 
