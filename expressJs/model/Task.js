1ba38b0f273d7534c52f5883e19f3f2e95d7adb5
kunaldhongade <kunaldhongade1@gmail.com>
2024-02-29 17:42:14 +0530

    update: adding Task

--- expressJs/model/Task.js
+++ expressJs/model/Task.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
 const taskSchema = new Schema({
-  title: { type: String, require: true },
-  status: { type: Boolean, require: true },
-  date: { type: Date, default: Date.now, require: true },
+  title: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    require: true,
+  },
 });
 
 exports.Task = mongoose.model("Task", taskSchema);
