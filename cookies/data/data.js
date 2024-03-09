0290295be17486d202a0f5e45afdcf2a815a2ebc
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-09 17:34:24 +0530

    update: adding new pkg

---
+++ cookies/data/data.js
@@ -0,0 +1,38 @@
+let users = [
+  {
+    id: 1,
+    name: "Kunal",
+    password: "password",
+    role: "admin",
+  },
+  {
+    id: 2,
+    name: "Raju",
+    password: "password",
+    role: "user",
+  },
+  {
+    id: 3,
+    name: "Sarah",
+    password: "password",
+    role: "user",
+  },
+  {
+    id: 4,
+    name: "George",
+    password: "password",
+    role: "user",
+  },
+  {
+    id: 5,
+    name: "ninja",
+    password: "password",
+    role: "user",
+  },
+];
+
+let mobiles = ["Apple iPhone 11", "Samsung Galaxy 9", "Google Pixel 4"];
+let laptops = ["HP 23x", "Dell Inspire", "Apple Macbook Air"];
+let offers = ["5% off on mobile", "8% cashback on laptops"];
+
+module.exports = { users, mobiles, laptops, offers };
