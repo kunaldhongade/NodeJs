6cf8e32dd9020bc8db25efe1c638894ee44082ee
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-19 12:29:52 +0530

    Update: adding encrypt with private key

--- Concept/cryptography/encrypt.js
+++ Concept/cryptography/encrypt.js
@@ -5,4 +5,10 @@ function encryptWithPublicKey(publicKey, message) {
   return crypto.publicEncrypt(publicKey, bufferMessage);
 }
 
+function encryptWithPrivateKey(privateKey, message) {
+  const bufferMessage = Buffer.from(message, "utf8");
+  return crypto.privateEncrypt(privateKey, bufferMessage);
+}
+
 module.exports.encryptWithPublicKey = encryptWithPublicKey;
+module.exports.encryptWithPrivateKey = encryptWithPrivateKey;
