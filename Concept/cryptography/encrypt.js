3f88d51479d19f78ca5d5df31cdfaa4ca3dbb8d7
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-19 11:55:08 +0530

    Add: encrypt message using publickey signature

---
+++ Concept/cryptography/encrypt.js
@@ -0,0 +1,8 @@
+const crypto = require("crypto");
+
+function encryptWithPublicKey(publicKey, message) {
+  const bufferMessage = Buffer.from(message, "utf-8");
+  return crypto.publicEncrypt(publicKey, bufferMessage);
+}
+
+module.exports.encryptWithPublicKey = encryptWithPublicKey;
