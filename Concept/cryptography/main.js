30467b786d732116329c6f455797dc8b326e0e7e
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-19 12:12:47 +0530

    Update: Adding decrypt method

--- Concept/cryptography/main.js
+++ Concept/cryptography/main.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const encrypt = require("./encrypt");
+const decrypt = require("./decrypt");
 
 const publicKey = fs.readFileSync(__dirname + "/id_rsa_pub.pem", "utf8");
 const message = "Super Secret Message";
@@ -7,3 +8,13 @@ const message = "Super Secret Message";
 const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, message);
 
 console.log(encryptedMessage.toString());
+
+const privateKey = fs.readFileSync(__dirname + "/id_rsa_priv.pem", "utf8");
+
+const decryptedMessage = decrypt.decryptWithPrivatekey(
+  privateKey,
+  encryptedMessage
+);
+
+// convert the buffer to a string and print the message
+console.log(decryptedMessage.toString());
