4cd35a9382ef2733c90a7bea1fa0bf298ae140e5
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-25 18:14:34 +0530

    update: verify identify using hash

--- Concept/cryptography/verifyIdentity.js
+++ Concept/cryptography/verifyIdentity.js
@@ -6,3 +6,24 @@ const receivedData = require("./signMessage").packageOfDataToSend; // this is th
 const publicKey = fs.readFileSync(__dirname + "/id_rsa_pub.pem", "utf8");
 
 const hash = crypto.createHash(receivedData.algorithm);
+
+const decryptedMessage = decrypt.decryptWithPublickey(
+  publicKey,
+  receivedData.signedAndEncryptedData
+);
+
+const decryptedMessageHex = decryptedMessage.toString();
+
+const hashOfOriginal = hash.update(JSON.stringify(receivedData.originalData));
+
+const hashOfOriginalHex = hash.digest("hex");
+
+if (hashOfOriginalHex === decryptedMessageHex) {
+  console.log(
+    "Success! The data has not been tampered with and the sender is valid"
+  );
+} else {
+  console.log(
+    "Uh oh... Someone is trying to manipulate the data or someone else is manipulated it"
+  );
+}
